ba71caade1641d3835ea7160c01c527d0f0acc0a
KrazyMiner001 <KrazyMiner001@outlook.com>
2024-03-18 17:01:43 +0200

    added an item

--- remappedSrc/com/example/ExampleMod.java
+++ remappedSrc/com/example/ExampleMod.java
@@ -2,11 +2,6 @@ package com.example;
 
 import net.fabricmc.api.ModInitializer;
 
-import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
-import net.minecraft.item.Item;
-import net.minecraft.registry.Registries;
-import net.minecraft.registry.Registry;
-import net.minecraft.util.Identifier;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,7 +10,6 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
     public static final Logger LOGGER = LoggerFactory.getLogger("modid");
-	public static final Item CUSTOM_ITEM = new Item(new FabricItemSettings());
 
 	@Override
 	public void onInitialize() {
@@ -24,6 +18,5 @@ public class ExampleMod implements ModInitializer {
 		// Proceed with mild caution.
 
 		LOGGER.info("Hello Fabric world!");
-		Registry.register(Registries.ITEM, new Identifier("tutorial", "custom_item"), CUSTOM_ITEM);
 	}
 }
