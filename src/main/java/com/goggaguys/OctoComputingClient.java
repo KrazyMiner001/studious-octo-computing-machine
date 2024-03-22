7115540de8fd9275ddc6c18bf05980b1d86ab567
KrazyMiner001 <krazyminer001@outlook.com>
2024-03-22 06:36:36 +0200

    made finnian sapling not have black background

--- src/main/java/com/goggaguys/OctoComputingClient.java
+++ src/main/java/com/goggaguys/OctoComputingClient.java
@@ -2,10 +2,15 @@ package com.goggaguys;
 
 import com.goggaguys.block.OctoBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class OctoComputingClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+		BlockRenderLayerMap.INSTANCE.putBlock(OctoBlocks.FINNIAN_SAPLING, RenderLayer.getCutout());
+		
+		OctoComputing.LOGGER.info("Initializing Client");
 	}
 }
